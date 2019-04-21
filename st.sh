fdebe8ee4cd2b411725f0cd368cb2a24f666eed2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-04-21 07:31:46 +0100

    st.sh

---
+++ st.sh
@@ -0,0 +1,11 @@
+# 20-4-2019 JHZ
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add doc
+git commit -m "Documents and auxiliary files"
+git add st.sh
+git commit -m "st.sh"
+git push
