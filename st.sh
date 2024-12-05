4977dd87b734d468eead506f396bb64a09932ecb
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-12-05 18:04:12 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,11 +1,9 @@
-# 7-8-2019 JHZ
+# 5-12-2024 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
 git add README.md
 git commit -m "README"
-git add misc.md
-git commit -m "miscellaneous resources"
 git add st.sh
 git commit -m "st.sh"
 git push
